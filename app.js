0b8899b158d7ab1e7bc23b1196981ee9525c51cd
Hamzaansari <hamzaansaree15@gmail.com>
2022-07-13 02:42:48 +0500

    HALF ASSGMENT COMPLETED

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// function today(){
+//     document.write(new Date());
+// }
+// today();
+// function first(a,b){
+//     alert(a+b);
+// }
+// var a = prompt("Enter your first name");
+// var b = prompt("Enter your last name");
+// first(a,b);
+// function sum(a,b){
+//    document.write(a + b);
+// }
+// var a = +prompt("Enter a number");
+// var b = +prompt("Enter a number");
+// sum(a,b);
+// function calculation(a,b,c){
+//    if(c === "+"){
+//     document.write(a + b);
+//     }else if(c === "-"){
+//         document.write(a - b);
+//     }else if(c === "*"){
+//         document.write(a * b);
+//     }
+// }
+// var num1 = +prompt("Enter a number");
+// var num2 = +prompt("Enter a number");
+// var oper = +prompt("select a operater '+''-'''*");
+// calculation(a,b,c);
+// function square(a){
+//    document.write(a*a);
+
+// }
+// var a = +prompt("Enter a number");
+// square(a);
